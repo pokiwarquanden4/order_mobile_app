c518c6927dab6f25ea84b5524feafc830a461cf8
pokiwarquanden <pokiwarquanden4@gmail.com>
2024-05-11 19:36:04 +0700

    First Commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -17,7 +17,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: 'List',
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
           ),
